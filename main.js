ea292f5eef0eb8ab6034fea505114230864e05b9
yingran2 <71774106+yingran2@users.noreply.github.com>
2020-11-08 00:11:37 -0500

    Assignment 6B featurn implement

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Limits amount of checkboxes user can click
+//limits glaze user can choose
 var flavor = document.getElementsByName('flavor');
 var limit = 0;
 for (var i = 0; i < flavor.length; i++) {
@@ -6,24 +6,12 @@ for (var i = 0; i < flavor.length; i++) {
 }
 
 
-function checkFlavor() {
-  if (this.checked) {
-    if (limit < 2) {
-      limit++
-    } else {
-      this.checked = false;
-    }
-  } else {
-    limit--;
-  }
-}
-
-//Updates number next to cart icon
+//Updates iterm number
 function updateCartAmount() {
   document.getElementById("cart").innerHTML = order.length;
 }
 
-//Creates array that populates cart
+//Creates iterm
 if (localStorage.getItem('myOrder')) {
   var order = JSON.parse(localStorage.getItem('myOrder'));
   updateCartAmount();
@@ -55,10 +43,19 @@ function updateCart() {
   if (order.length > 0) {
     for (var i = 0; i < order.length; i++) {
       var orderHtml = document.createElement("div");
+      orderHtml.className = 'orderspace';
       orderHtml.setAttribute('data-order-number', i);
       orderHtml.innerHTML = '<button id="close" onclick="removeFromCart()"></button><div class="inBasket"><h3>' + order[i].amount + '</h3><p span class="flavorChoice">' + order[i].flavors.join(', ') + '</p><p span class="orderPrice">' + order[i].price + '</p></div><div class="border long"></div>'
       document.getElementById('yourOrder').appendChild(orderHtml);
     }
+  }
+}
+
+//Changes cart text and button when cart is empty
+function checkout() {
+  if (order.length == 0) {
+    document.getElementById('checkoutNow').innerHTML = "Your cart is empty.";
+    document.getElementById('checkoutButton').innerHTML = 'shop now';
   }
 }
 
@@ -71,26 +68,6 @@ function removeFromCart() {
   localStorage.setItem('myOrder', JSON.stringify(order));
 }
 
-//Supports dropdownChange() function, exposes divs
-function showFlavors() {
-  if (document.getElementById('flavors').style.display === "none") {
-    document.getElementById('flavors').style.display = "block";
-    document.getElementById("moreDescription").style.display = "block";
-    document.getElementById('main').src = 'images/product1.jpg';
-  }
-}
-
-//Supports dropdownChange() function, hides divs
-function hideFlavors() {
-  if (document.getElementById('flavors').style.display === "block") {
-    document.getElementById('flavors').style.display = "none";
-    document.getElementById('moreDescription').style.display = "none";
-    document.getElementById('main').src = 'images/product1.jpg';
-    for (var i = 0; i < flavor.length; i++) {
-      flavor[i].checked = false;
-    }
-  }
-}
 
 //Change price for certain pack
 function dropdownChange() {
